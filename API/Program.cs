28b60d58fe9665ae035e6c6d1b0c8110e7874f18
llp16 <lilanpeng16@126.com>
2022-12-23 20:49:49 -0600

    Section 3 done

--- API/Program.cs
+++ API/Program.cs
@@ -13,14 +13,24 @@ builder.Services.AddDbContext<DataContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(opt => 
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(); 
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
